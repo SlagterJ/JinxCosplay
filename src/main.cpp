d4361db70b6a5b7042ba633f886f229b3abc9b85
Jordy Slagter <slagterj2001@gmail.com>
2025-05-29 12:14:02 +0200

    add pin definitions

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,37 @@
 #include <Arduino.h>
 #include <Adafruit_Neopixel.h>
 
+/**
+ * Arduino data pin for the barrel neopixels
+ */
+#define PIN_NEOPIXELS_BARREL =      2
+/**
+ * Arduino data pin for the clock neopixels
+ */
+#define PIN_NEOPIXELS_CLOCK =       3
+/**
+ * Arduino data pin for the horns neopixels
+ */
+#define PIN_NEOPIXELS_HORNS =       4
+/**
+ * Arduino data pin for the jaw neopixels
+ */
+#define PIN_NEOPIXELS_JAW =         5
+/**
+ * Arduino data pin for the motor switch
+ */
+#define PIN_MOTOR_SWITCH =          13
+
+/**
+ * Setup function runs once on startup
+ */
 void setup() {
   // write your initialization code here
 }
 
+/**
+ * Loop function runs for the remainder lifetime
+ */
 void loop() {
   // write your code here
 }
