b472a39208374f6648769e68e27f8e24f920bd8c
Jordy Slagter <slagterj2001@gmail.com>
2025-05-29 17:12:24 +0200

    add testing logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,61 @@
 #include <Arduino.h>
 #include <Adafruit_Neopixel.h>
 
+constexpr uint8_t NUM_NEOPIXELS_BARREL =      40;
 /**
  * Arduino data pin for the barrel neopixels
  */
-#define PIN_NEOPIXELS_BARREL =      2
+constexpr uint8_t PIN_NEOPIXELS_BARREL =      2;
 /**
  * Arduino data pin for the clock neopixels
  */
-#define PIN_NEOPIXELS_CLOCK =       3
+constexpr uint8_t PIN_NEOPIXELS_CLOCK =       3;
 /**
  * Arduino data pin for the horns neopixels
  */
-#define PIN_NEOPIXELS_HORNS =       4
+constexpr uint8_t PIN_NEOPIXELS_HORNS =       4;
 /**
  * Arduino data pin for the jaw neopixels
  */
-#define PIN_NEOPIXELS_JAW =         5
+constexpr uint8_t PIN_NEOPIXELS_JAW =         5;
 /**
  * Arduino data pin for the motor switch
  */
-#define PIN_MOTOR_SWITCH =          13
+constexpr uint8_t PIN_MOTOR_SWITCH =          13;
+
+Adafruit_NeoPixel barrelStrip(NUM_NEOPIXELS_BARREL, PIN_NEOPIXELS_BARREL);
 
 /**
- * Setup function runs once on startup
+ * Boolean to keep track if the motor switch was pressed the previous cycle used to prevent double inputs
+ */
+bool wasMotorSwitchPressedPreviousCycle = false;
+
+/**
+ * Setup function runs once on start
  */
 void setup() {
-  // write your initialization code here
+  barrelStrip.begin();
+  // clear the strip
+  barrelStrip.show();
 }
 
 /**
  * Loop function runs for the remainder lifetime
  */
 void loop() {
-  // write your code here
+  /**
+   * if the motor switch is pressed this cycle
+   */
+  /*
+  const bool isMotorSwitchPressed = digitalRead(PIN_MOTOR_SWITCH) == LOW;
+
+  if (isMotorSwitchPressed && wasMotorSwitchPressedPreviousCycle == false) {
+    wasMotorSwitchPressedPreviousCycle = true;
+  }
+  */
+
+  for (int i = 0; i < NUM_NEOPIXELS_BARREL; i++) {
+    barrelStrip.setPixelColor(i, Adafruit_NeoPixel::Color(255, 0, 0));
+    delay(200);
+  }
 }
